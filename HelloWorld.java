dfaa2d2a5134c6bc039cedbf879fe02c4ca50845
23maya <23maya.henkel@boiseschools.net>
2020-08-20 18:08:24 +0000

    Added picture

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+//Maya Henkel
+//AP CSA
+//8-20-2020
+
+public class HelloWorld{
+  public static void main(String args[]){
+    /* a heart shape outlined in hearts with some sentences on the side.
+    */
+    System.out.println("────(♥)(♥)(♥)────(♥)(♥)(♥) __ ɪƒ ƴσυ'ʀє αʟσηє,");
+    System.out.println("──(♥)██████(♥)(♥)██████(♥) ɪ'ʟʟ ɓє ƴσυʀ ѕɧα∂σѡ.");
+    System.out.println("─(♥)████████(♥)████████(♥) ɪƒ ƴσυ ѡαηт тσ cʀƴ,");
+    System.out.println("─(♥)██████████████████(♥) ɪ'ʟʟ ɓє ƴσυʀ ѕɧσυʟ∂єʀ.");
+    System.out.println("──(♥)████████████████(♥) ɪƒ ƴσυ ѡαηт α ɧυɢ,");
+    System.out.println("────(♥)████████████(♥) __ ɪ'ʟʟ ɓє ƴσυʀ ρɪʟʟσѡ.");
+    System.out.println("──────(♥)████████(♥) ɪƒ ƴσυ ηєє∂ тσ ɓє ɧαρρƴ,");
+    System.out.println("────────(♥)████(♥) __ ɪ'ʟʟ ɓє ƴσυʀ ѕɱɪʟє.");
+    System.out.println("─────────(♥)██(♥) ɓυт αηƴтɪɱє ƴσυ ηєє∂ α ƒʀɪєη∂,");
+    System.out.println("───────────(♥) __ ɪ'ʟʟ ʝυѕт ɓє ɱє.");
+
+  }
+
+
+
+}
